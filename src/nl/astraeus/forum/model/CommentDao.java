ed2ab553c63e04f113fd16bc1a2e0e79d16c41ca
Rien Nentjes <rien@nentjes.com>
2012-04-08 22:18:41 +0200

    Added all required files

---
+++ src/nl/astraeus/forum/model/CommentDao.java
@@ -0,0 +1,11 @@
+package nl.astraeus.forum.model;
+
+import nl.astraeus.prevayler.PrevaylerDao;
+
+/**
+ * User: rnentjes
+ * Date: 3/28/12
+ * Time: 3:46 PM
+ */
+public class CommentDao extends PrevaylerDao<Comment> {
+}

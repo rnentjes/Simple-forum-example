027ea52fcb3fae8e3752db4197e4e7a8622d009d
Rien Nentjes <rien@nentjes.com>
2012-06-02 20:41:17 +0200

    Added Jetty, ext libs
    More REST
    Merge branch 'master' of github.com:rnentjes/Simple-forum-example
    
    Conflicts:
    	src/nl/astraeus/forum/web/ForumServlet.java

--- src/nl/astraeus/forum/web/HttpServer.java
+++ src/nl/astraeus/forum/web/HttpServer.java
@@ -1,6 +1,11 @@
 package nl.astraeus.forum.web;
 
 import nl.astraeus.http.SimpleWebServer;
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.nio.SelectChannelConnector;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 
 /**
  * User: rnentjes
@@ -9,19 +14,52 @@ import nl.astraeus.http.SimpleWebServer;
  */
 public class HttpServer {
     
-    public static void main(String [] args) throws Exception {
+    public static void main(String args []) throws Exception {
         int port = 8080;
 
         if (args.length == 1) {
             port = Integer.parseInt(args[0]);
         }
 
+        startJetty(port);
+    }
+
+    public static void startSimpleServer(int port) {
         SimpleWebServer server = new SimpleWebServer(port);
 
         server.addServlet(new ResourceServlet(), "/resources/*");
         server.addServlet(new ForumServlet(), "/*");
 
         server.start();
+    }
+
+    public static void startJetty(int port) throws Exception {
+        Server server = new Server(port);
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+
+        context.setContextPath("/");
+
+        server.setHandler(context);
+
+        ServletHolder resourceServlet = new ServletHolder(new ResourceServlet());
+        ServletHolder forumServlet = new ServletHolder(new ForumServlet());
+
+        context.addServlet(resourceServlet, "/resources/*");
+        context.addServlet(forumServlet, "/*");
+
+        server.start();
+
+        for (Connector con : server.getConnectors()) {
+            System.out.println("Found connector: "+con);
+            if (con instanceof SelectChannelConnector) {
+                SelectChannelConnector scc = (SelectChannelConnector)con;
+
+                System.out.println("\tAcceptors: "+scc.getAcceptors());
+                System.out.println("\tQueue size: "+scc.getAcceptQueueSize());
+                System.out.println("\tThreads: "+scc.getThreadPool().getThreads());
+            }
+        }
     }
 
 }
